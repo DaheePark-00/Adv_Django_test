4b3334487ef2fdc4763a7aa2cd3c3967de55b2ed
dahee0628 <ekgml3219@gmail.com>
2022-03-17 13:56:34 +0900

    add testcase

--- sparta/tests/test_view.py
+++ sparta/tests/test_view.py
@@ -6,4 +6,4 @@ class TestView(TestCase):
     def test_add_view(self) -> None:
         result = self.client.get("/api/add", {"a":1, "b":3})
         self.assertEqual(result.status_code, 200)
-        self.assertEqual(result.json(), {"result":4})
+        self.assertEqual(result.jsgit on(), {"result":4})
